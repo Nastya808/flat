bddf463e898a36d7e89778513758f5821b51d9b1
Nastya808 <57590462+Nastya808@users.noreply.github.com>
2023-12-07 15:04:47 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+namespace Flat
+{
+    public partial class MainWindow : Window
+    {
+        private UIElement selectedElement;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mapCanvas.MouseLeftButtonDown += MapCanvas_MouseLeftButtonDown;
+            mapCanvas.MouseMove += MapCanvas_MouseMove;
+            mapCanvas.MouseLeftButtonUp += MapCanvas_MouseLeftButtonUp;
+        }
+
+        private void MapCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            selectedElement = e.Source as UIElement;
+        }
+
+        private void MapCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+            if (selectedElement != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                double newX = e.GetPosition(mapCanvas).X - selectedElement.RenderSize.Width / 2;
+                double newY = e.GetPosition(mapCanvas).Y - selectedElement.RenderSize.Height / 2;
+
+                Canvas.SetLeft(selectedElement, newX);
+                Canvas.SetTop(selectedElement, newY);
+            }
+        }
+
+        private void MapCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            selectedElement = null;
+        }
+    }
+}
